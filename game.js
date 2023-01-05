4891d49c353c56d2950574049c1a485c25aaeaa9
Oumar <baarryoumar@gmail.com>
2023-01-05 00:05:54 -0500

    step 3 done but audio not working

--- game.js
+++ game.js
@@ -1,14 +1,19 @@
 
-var buttonColors = ["red", "blue", "green", "yellow"];
+var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 
 function nextSequence(){
 
   var randomNumber = Math.floor(Math.random() * 4);
   
-  var randomChosenColor = buttonColors[randomNumber];
+  var randomChosenColour = buttonColours[randomNumber];
 
-  gamePattern.push(randomChosenColor);
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
 
 nextSequence();
